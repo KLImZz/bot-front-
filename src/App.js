1f4f4a90a6f57c2c0cce657a6000ec832b5d8044
KLImZz <fonegenna@mail.ru>
2023-03-05 11:36:35 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {React, useEffect} from 'react'
+import {Routes, Route} from "react-router-dom";
+import {Header} from './components/Header/Header'
+import {ProductList} from './components/ProductList/ProductList'
+import {Form} from './components/Form/Form'
+const tg = window.Telegram.WebApp;
 
 function App() {
+  useEffect(()=>{
+    tg.ready()
+  }, [])
+  const closeEvent =()=>{
+    tg.close()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='container' >
+      <Header/>
+      <Routes> 
+        <Route index element = {<ProductList/>}/>
+        <Route path = {'/form'} element = {<Form/>}/>
+      </Routes>
+      <button className='btn' onClick={closeEvent}>Закрыть</button>
+     </div>
     </div>
   );
 }
+ 
 
 export default App;
